8e7ac2af8ad85b1355bb3abe5183825c8361eec8
Gan-Ming-Zhong <119864571+Gan-Ming-Zhong@users.noreply.github.com>
2022-12-05 21:09:38 +0800

    Create Question 2_PSNR.py
    If the condition is mse==0, the video won't summarize much.
    So I set the condition to mse<= 1e-6

---
+++ Question 2_PSNR.py
@@ -0,0 +1,61 @@
+import numpy as np
+import cv2
+import math
+import imageio
+from matplotlib import pyplot as plt
+
+def summarizeVideo(videoFile, duration):
+    
+    cap = cv2.VideoCapture(videoFile)
+    framesList=[]
+    total_mse=0
+    PSNR=0
+    
+    ret, frame = cap.read()
+    count=0
+    #framesList.append(frame)
+    cv2.imwrite("frame%d.jpg" % count, frame)
+    
+    while(cap.isOpened()):
+        prev_frame=frame
+        ret, frame = cap.read()
+
+        if ret==True:
+            i,j,c=frame.shape
+            mse = np.mean(((frame - prev_frame) ** 2)/(c*i*j))
+            #print("MSE= ",mse)
+            
+            # if mse==0, there is not much changes  
+            if mse<= 1e-6 :
+                PSNR+=100
+            else:
+                PIXEL_MAX = 255.0
+                PSNR +=10 * math.log10((PIXEL_MAX **2)/ mse)
+                count+=1
+                cv2.imwrite("frame%d.jpg" % count, frame)
+                framesList.append("frame%d.jpg" % count)
+                
+            
+        else:
+            break
+            
+    print("PSNR= ",PSNR)
+    cap.release()
+
+
+    return framesList
+
+
+
+fig=plt.figure()
+
+videoFile = 'Marvels Iron Man 3 Domestic Trailer 2 (OFFICIAL).mp4'
+framesList=summarizeVideo(videoFile,10) #10 minutes
+# Visualize the first 20 frames
+
+for i, imf in enumerate(framesList[:20]):
+    
+    fig.add_subplot(4, 5, 1+i)
+    im=cv2.imread(imf)
+    plt.imshow(im)
+    plt.axis('off')
